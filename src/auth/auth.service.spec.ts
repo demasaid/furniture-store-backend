1688b9ad06433a58ff636b3b16c3237fcf325631
Dema Said <demasaid@mail.tau.ac.il>
2026-07-14 18:45:39 +0300

    fix authentication unit test dependencies

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,15 +1,54 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../users/user.service';
 import { AuthService } from './auth.service';
+import { OtpService } from './otp.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const mockUsersService = {
+    findUserByEmail: jest.fn(),
+    findUserByPhone: jest.fn(),
+    findAuthUserById: jest.fn(),
+    createUser: jest.fn(),
+  };
+
+  const mockOtpService = {
+    createOtp: jest.fn(),
+    sendOtp: jest.fn(),
+    verifyOtp: jest.fn(),
+  };
+
+  const mockJwtService = {
+    signAsync: jest.fn(),
+  };
+
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+    const module: TestingModule =
+      await Test.createTestingModule({
+        providers: [
+          AuthService,
+          {
+            provide: UsersService,
+            useValue: mockUsersService,
+          },
+          {
+            provide: OtpService,
+            useValue: mockOtpService,
+          },
+          {
+            provide: JwtService,
+            useValue: mockJwtService,
+          },
+        ],
+      }).compile();
 
     service = module.get<AuthService>(AuthService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
